15bf447cec6abb5a1e3459b1caf887baf98adb0d
iravinderr <imravinderr@gmail.com>
2024-03-02 11:41:55 +0530

    minute changes

--- src/config/cloudinary.config.js
+++ src/config/cloudinary.config.js
@@ -7,11 +7,11 @@ const cdConnect = async() => {
             cloud_name: process.env.CLOUD_NAME,
             api_key: process.env.API_KEY,
             api_secret: process.env.API_SECRET,
-        })
+        });
         console.log("CLOUDINARY CONNECTION SUCCESSFULL");
     }
     catch(error){
-        console.log("!! ERROR IN CLOUDINARY CONNECTION !!");
+        console.log("!!! ERROR IN CLOUDINARY CONNECTION !!!");
         console.error(error);
         process.exit(1);
     }

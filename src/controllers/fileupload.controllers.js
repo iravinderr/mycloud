14a1fd59157da9ce4fe3b632acec69d4e1f669f4
iravinderr <imravinderr@gmail.com>
2024-03-02 19:16:04 +0530

    resource_type made auto

--- src/controllers/fileupload.controllers.js
+++ src/controllers/fileupload.controllers.js
@@ -34,6 +34,7 @@ exports.localFileUpload = async(req, res) => {
 
 const uploadToCloudinary = async(file, folder) => {
     const options = {folder};
+    options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
